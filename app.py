b1818d2851a9cfe110e238e5a68208923a720773
EC2 Default User <ec2-user@ip-10-0-1-166.ap-southeast-2.compute.internal>
2026-03-25 12:48:33 +0000

    AddedGET/DELETE/POST/GET endpoint

--- app.py
+++ app.py
@@ -17,6 +17,10 @@ app = Flask(__name__)
 dynamodb = boto3.resource("dynamodb", region_name="ap-southeast-2")
 table = dynamodb.Table("smartparcel-parcels")
 
+s3 = boto3.client("s3")
+BUCKET_NAME = "smartparcel-photos-20240004591"
+
+
 #User-Authentication
 def get_user():
     return request.headers.get("User")
@@ -53,16 +57,14 @@ def create_parcel():
         "sender": data["sender"], 
         "receiver": data["receiver"],
         "address": data["address"],
-        "status": "created",
-        "created_at": current_time()
+	"status": "parcel has been created",
     }
 
     try:
         table.put_item(Item=item)
 
         return jsonify({
-            "message": "Parcel created",
-            "parcel_id": parcel_id
+            "The parcel id is: ": parcel_id
         }), 201
 
     except Exception as e:
@@ -80,16 +82,16 @@ def get_parcel(parcel_id):
         return jsonify({"error": "unauthorized access"}), 401
    
     try:
-        response = table.get_item(Key={"parcel_id": parcel_id})
+        exists  = table.get_item(Key={"parcel_id": parcel_id})
 
-        if "Item" not in response:
+        if "Item" not in exists:
             return jsonify({"error": "Parcel not found"}), 404
 
-        return jsonify(response["Item"]), 200
+        return jsonify (exists["Item"]), 200
 
     except Exception as e:
         return jsonify({
-            "error": "Server error",
+            "error": "ERROR",
             "details": str(e)
         }), 500
 
@@ -110,6 +112,89 @@ def update_parcel(parcel_id):
 	if not status or status not in parcel_status:
 		return jsonify({"error": "Invalid status"}),404
 
+	try:
+		exists  = table.get_item(Key={"parcel_id": parcel_id})
+	
+		if "Item" not in exists:
+			return jsonify({"error": "No parcel created"}), 404
+	
+		table.update_item(
+			Key={"parcel_id": parcel_id},
+			UpdateExpression= "set #s =  :s",
+			ExpressionAttributeNames={"#s": "status"},
+			ExpressionAttributeValues={":s": status}
+			)
+		
+		return jsonify({
+			"The parcel status has been updated to: ": status
+			}),200
+
+	except Exception as e:
+		return jsonify({
+			"error": "ERROR",
+			"details": str(e)
+		}), 500
+	
+#GET List Parcels 
+@app.route("/api/parcels", methods = ["GET"])
+def list_parcels():
+	user = get_user()
+
+	if user != "admin":
+		return jsonify({"error": "unauthorized access"}),401
+	
+	status_filer = request.args.get("status")
+	try:
+		exists = table.scan()
+		items= exists["Items"]
+
+		return jsonify ({
+			"count": len(items),
+			"parcels" : items
+			}), 200
+	except:
+		return jsonify ({"error": "ERROR"}), 500
+			
+#DELETE Cancel Parcels 
+@app.route("/api/parcels/<parcel_id>", methods = ["DELETE"])
+def delete_parcel(parcel_id):
+    user = get_user()
+
+
+    if user != "admin":
+        return jsonify({"error": "unauthorized access"}), 401
+
+    try:
+        exists = table.get_item(Key={"parcel_id": parcel_id})
+
+        if "Item" not in exists:
+            return jsonify({"error": "Parcel has not been created"}), 404
+
+        parcel = exists["Item"]
+
+
+        if parcel["status"] != "created":
+            return jsonify({"error": "Cannot cancel parcel"}), 400
+
+        table.delete_item(Key={"parcel_id": parcel_id})
+
+        return jsonify({"message": "Parcel cancelled"}), 200
+
+    except:
+        return jsonify({"error": "ERROR"}), 500
+
+#POST UPLOAD PHOTO
+@app.route("/api/parcels/<parcel_id>/photo", methods=["POST"])
+def upload_parcel(parcel_id):
+     user = get_user()
+
+     if user != "driver":
+          return jsonify({"error": "unauthorized access"}), 401
+     
+     photo = request.files["photo"]
+
+     if "photo" not in request.files:
+          return jsonify({"error": "There's no photo uploaded"}), 400
 
 if __name__ == "__main__":
      app.run(host="0.0.0.0",port=8080)
